c48c8d657ddb3583e0baadbc149f6cd7e0c337a7
Carlos Daniel <carlos.daniel@devsquad.email>
2022-01-26 23:04:26 -0300

    feat(game): start game with simple validation
    yes this is pretty xgh because i am sleepy right now. will refactor it as soon as possible

--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,12 @@ import isNil from 'lodash/isNil'
 import omitBy from 'lodash/omitBy'
 
 export const cleanObject = (object: object) => omitBy(object, isNil)
+
+export const makeId = (length = 6) => {
+  let text = ''
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return text
+}
