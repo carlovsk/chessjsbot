43dac3a63a5d296940a7440c92b47782c5fc0790
Carlos Daniel <carlos.daniel@devsquad.email>
2022-01-28 20:03:46 -0300

    feat(ddb): verify if user exists before saving and save `startedAt` attribute

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,13 +1,11 @@
-import { PutItem } from "../ddb"
-import { sendMessage } from "../services"
+import { userById } from '../db/queries'
+import { saveUser } from '../db/functions'
+import { sendMessage } from '../services'
 
 export default async (payload): Promise<void> => {
   const { id } = payload.message.chat
   await sendMessage('Welcome!\n\nThis is the HarvestJs Bot! It\'s an open source project made with JavaScript.\n\nYou can start a game by running the command `/newgamebot`.\n\nHave fun!', id)
 
-  await PutItem({
-    pk: 'user',
-    sk: `${id}`,
-    ...payload.message.from
-  })
+  const user = await userById(id.toString())
+  if (!user) await saveUser(payload.message.from)
 }
