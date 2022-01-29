70037381e2bfec26162fef78199e22cda326af8c
Carlos Daniel <carlos.daniel@devsquad.email>
2022-01-28 21:36:18 -0300

    feat(commands): improve /start message

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -2,9 +2,20 @@ import { userById } from '../db/queries'
 import { saveUser } from '../db/functions'
 import { sendMessage } from '../services'
 
+const message = `
+Welcome!
+
+I am the *ChessJs Bot*! An open-source bot built with JavaScript and deployed on AWS. If you want to know more about it, type \`/readme\`.
+
+You can start a new game against me by running the command \`/newgamebot\`.
+You can also see the whole list of commands by running the command \`/help\`.
+
+Have fun!
+`
+
 export default async (payload): Promise<void> => {
   const { id } = payload.message.chat
-  await sendMessage('Welcome!\n\nThis is the HarvestJs Bot! It\'s an open source project made with JavaScript.\n\nYou can start a game by running the command `/newgamebot`.\n\nHave fun!', id)
+  await sendMessage(message, id)
 
   const user = await userById(id.toString())
   if (!user) await saveUser(payload.message.from)
